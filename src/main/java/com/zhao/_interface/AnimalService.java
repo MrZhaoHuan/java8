5131909b2d28790b54b544775f3b68f729022793
zhaohuan <1101006260@qq.com>
2018-09-02 22:06:01 +0800

    FunctionalInterface(函数式接口)

---
+++ src/main/java/com/zhao/_interface/AnimalService.java
@@ -0,0 +1,12 @@
+package com.zhao._interface;
+
+/**
+ * @创建人 zhaohuan
+ * @邮箱 1101006260@qq.com
+ * @创建时间 2018-09-02 20:04
+ * @描述    函数式接口
+ **/
+@FunctionalInterface
+public interface AnimalService {
+    void eat(String food);
+}
